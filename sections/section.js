93ebfc3fe4a699ad1d3f45027599c81dc6c4f475
blekk111 <jd.sancheza@javeriana.edu.co>
2024-08-02 08:22:29 -0500

    preparacion

--- sections/section.js
+++ sections/section.js
@@ -5,3 +5,12 @@ function goToPage(page) {
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.navbar ul').classList.toggle('showing');
 });
+
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
